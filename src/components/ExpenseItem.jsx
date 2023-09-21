f6f20c82f0fdbcc2070cf9ff3b710f01cbfc3b0f
kibreab <kibreabteshome1993@gmail.com>
2023-09-21 17:02:55 +0300

    starting the project

---
+++ src/components/ExpenseItem.jsx
@@ -0,0 +1,15 @@
+ import React from 'react'
+ 
+ const ExpenseItem = () => {
+   return (
+   <div>
+    <div>march 28,2021</div>
+    <div>
+        <h2>Car insurance</h2>
+        <div>$294.67</div>
+    </div>
+   </div>
+   )
+ }
+ 
+ export default ExpenseItem
