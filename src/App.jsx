8dd232d099e7b3dfc65f9238eb999a98d5b879bf
kibreab <kibreabteshome1993@gmail.com>
2023-09-24 13:45:29 +0300

    organizing files

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
 import React from 'react'
 import Expenses from './components/Expenses/Expenses';
 
+
+
 const App = () => {
 
-  const expenses = [
+  const expenses = 
+    [
     {title: 'Household Expense', amount: 25000, date: new Date(2023, 8, 27)},
     {title: 'Travel Expense', amount: 1300, date: new Date(2023, 4, 27)},
     {title: 'Education Fees', amount: 5000, date: new Date(2023, 5, 27)}
-];
+    ];
 
 
   return (
     <div>
-    <Expenses items={expenses}/>
+     <Expenses items={expenses}/>
     </div>
   )
 }
