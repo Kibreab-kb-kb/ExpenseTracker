5de6dea25af3dc1e47dd21aa5c910605b5f95f1c
kibreab <kibreabteshome1993@gmail.com>
2023-10-22 09:32:23 +0300

    some update on the project

--- src/components/Expenses/ExpenseItem.jsx
+++ src/components/Expenses/ExpenseItem.jsx
@@ -2,8 +2,15 @@ import React from 'react'
 import './ExpenseItem.css'
 import ExpenseDate from './ExpenseDate'
 import Card from '../UI/Card'
- 
+import { useState } from 'react'
  const ExpenseItem = (props) => {
+
+  const [title,setTitle]=useState(props.title)
+  
+  const handleClick=()=>{
+    setTitle('KB')
+    console.log(title)
+  }
   
   
 
@@ -11,11 +18,12 @@ import Card from '../UI/Card'
    <Card className='expense-item'>
     <ExpenseDate date={props.date}/>
     <div className='expense-item__description'>
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
+        <button onClick={handleClick}>Click Here</button>
     </div>
    </Card>
    )
  }
  
- export default ExpenseItem
+ export default ExpenseItem 
