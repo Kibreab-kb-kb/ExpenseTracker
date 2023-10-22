9c9ef63464b7490200c27fff1971ea54bc81f69c
kibreab <kibreabteshome1993@gmail.com>
2023-10-22 19:14:36 +0300

    integrating two component

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,32 @@ import NewExpense from './components/NewExpense/NewExpense';
 
 const App = () => {
 
-  const expenses = 
-    [
-    {title: 'Household Expense', amount: 25000, date: new Date(2023, 8, 27)},
-    {title: 'Travel Expense', amount: 1300, date: new Date(2023, 4, 27)},
-    {title: 'Education Fees', amount: 5000, date: new Date(2023, 5, 27)},
-    {title: 'New Desk(Wooden)', amount: 450, date: new Date(2023, 6, 27)}
+  const expenses = [
+    {
+      id:'e1',
+    title: 'Household Expense', 
+    amount: 25000, 
+    date: new Date(2023, 8, 27)
+  },
+
+    {
+      id:'e2',
+      title: 'Travel Expense', 
+      amount: 1300, 
+      date: new Date(2023, 4, 27)
+    },
+    {
+      id:'e3',
+      title: 'Education Fees', 
+      amount: 5000, 
+      date: new Date(2023, 5, 27)
+    },
+    {
+      id:'e4',
+      title: 'New Desk(Wooden)', 
+      amount: 450, 
+      date: new Date(2023, 6, 27)
+    }
     ];
 
 
